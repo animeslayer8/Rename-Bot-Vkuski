62d7fe35fbf1040b87a79b3fe74172b35e5cb2e4
animeslayer8 <126319219+animeslayer8@users.noreply.github.com>
2023-04-11 20:06:26 +0530

    Update start.py
    Signed-off-by: animeslayer8 <126319219+animeslayer8@users.noreply.github.com>

--- plugins/start.py
+++ plugins/start.py
@@ -16,7 +16,7 @@ async def start(client, message):
         await db.add_user(user.id)             
     txt=f"👋 Hello Developer {user.mention} \n\nI am an Advance file Renamer and file Converter BOT with Custom thumbnail support.\n\nSend me any video or document !"
     button=InlineKeyboardMarkup([[
-        InlineKeyboardButton(" Developer ", callback_data='dev')
+        InlineKeyboardButton(" Developer ", callback_data='@Katsuki_warlord')
         ],[
         InlineKeyboardButton(' Updates', url='https://t.me/Anime_complex'),
         InlineKeyboardButton(' Support', url='https://t.me/Teamwarlords')
